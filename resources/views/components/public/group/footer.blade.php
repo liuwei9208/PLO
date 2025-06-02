c1314d697b8330fe1e21313e6b9983d498552105
liu wei <ryu@applink.co.jp>
2025-06-02 20:59:22 +0900

    グループTOP->フッター

--- resources/views/components/public/group/footer.blade.php
+++ resources/views/components/public/group/footer.blade.php
@@ -6,4 +6,16 @@
 </div>
 
 <footer class="footer" id="footer">
+  <nav class="footer__nav">
+    <div class="footer__nav-item"><a href="{{ route('public.group.home') }}">HOME</a></div>
+    <div class="footer__nav-item"><a href="{{ route('public.group.shop') }}">店舗一覧</a></div>
+    <div class="footer__nav-item"><a href="{{ url('https://hokkaido-tohoku.qzin.jp/group/hinaShizuku/1/') }}">求人情報</a></div>
+    <div class="footer__nav-item"><a href="{{ route('public.group.home') }}">グループTOP</a></div>
+  </nav>
+  <div class="footer__nav">
+    <div class="footer__nav-item"><a href="/privacy">個人情報保護方針</a></div>
+  </div>
+  <div class="footer__copyright">
+    Copyright © PLO Group All Rights Reserved.
+  </div>
 </footer>
