9ff65abd5c3d79edc6a9173f51a86a1592da86fd
liu wei <ryu@applink.co.jp>
2025-06-04 14:10:55 +0900

    キャスト詳細ページのレイアウトを調整し、メモ入力欄のコメントアウトを追加しました。また、Q&Aセクションの一部をコメントアウトし、表示ロジックを整理しました。

--- resources/views/admin/cast/detail.blade.php
+++ resources/views/admin/cast/detail.blade.php
@@ -518,18 +518,26 @@
               </span>
             </div>
           </div>
+          {{-- <div>
+            <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+              メモ
+            </label>
+            <textarea
+              name="memo"
+              class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+            >{{ $cast->memo }}</textarea>
+          </div>           --}}
           <div class="flex items-center gap-4 mb-6">
-            <label class="text-sm font-medium text-gray-700 dark:text-gray-400">
+          {{-- <div> --}}
+            <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
               {{ 'A'.$label }}
             </label>
-            <div class="relative z-20 w-full max-w-[380px] bg-transparent">
+            {{-- <div class="relative z-20 w-full max-w-[380px] bg-transparent"> --}}
               <textarea
                 name="a{{ $label }}"
                 class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
-              >
-                {{ isset($qas[$index]) ? $qas[$index]->answer : '' }}
-              </textarea>
-            </div>
+              >{{ isset($qas[$index]) ? $qas[$index]->answer : '' }}</textarea>
+            {{-- </div> --}}
           </div>
         @endforeach
     </div>
