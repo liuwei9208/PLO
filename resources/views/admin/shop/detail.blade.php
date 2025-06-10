dfc16ed50582e30c2d64ae49cfd0260b096641e9
liu wei <ryu@applink.co.jp>
2025-06-10 11:44:29 +0900

    ショップ詳細ページに営業時間とメモの入力フィールドを追加しました。これにより、ショップの運営情報をより詳細に管理できるようになりました。

--- resources/views/admin/shop/detail.blade.php
+++ resources/views/admin/shop/detail.blade.php
@@ -85,6 +85,43 @@
           >
         </div>
 
+        <!-- 営業時間 -->
+        <div class="mb-6">
+          <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+            営業時間
+          </label>
+          <div class="flex items-center gap-4">
+            <input
+              name="open_start"
+              type="time"
+              value="{{ $shop->open_start }}"
+              class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full max-w-[180px] rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+            >
+            <span class="text-gray-500">〜</span>
+            <input
+              name="open_end"
+              type="time"
+              value="{{ $shop->open_end }}"
+              class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full max-w-[180px] rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+            >
+          </div>
+        </div>
+
+        <!-- Memo -->
+        <div class="mb-6 w-full">
+          <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+            メモ
+          </label>
+          <textarea
+            name="memo"
+            type="text"
+            value="{{ $shop->memo }}"
+            rows="5"
+            class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 w-full  rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+          >
+          </textarea>
+        </div>
+        
         <!-- Email -->
         <div class="mb-6">
           <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
