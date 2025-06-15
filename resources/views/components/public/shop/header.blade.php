67b3fe6351ced8fed01224c7004f1dfab2f18bfd
liu wei <ryu@applink.co.jp>
2025-06-15 22:03:32 +0900

    ショップのヘッダーにおけるキャスト一覧へのリンクを、プレースホルダーから新たに設定したルートに更新しました。

--- resources/views/components/public/shop/header.blade.php
+++ resources/views/components/public/shop/header.blade.php
@@ -26,7 +26,7 @@
         <ul class="drawer-nav-list">
           <li><a href="{{ route('public.shop.home', $shop->slug) }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">TOPページ</a></li>
           <li><a href="{{ route('public.shop.event', $shop->slug) }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">イベント</a></li>
-          <li><a href="{{ url('#') }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">キャスト一覧</a></li>
+          <li><a href="{{ route('public.shop.castlist', $shop->slug) }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">キャスト一覧</a></li>
           <li><a href="{{ url('#') }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">口コミ一覧</a></li>
           @if($shop)
             <li><a href="{{ url('/' . $shop->slug . '/ranking') }}"><img src="{{ asset('assets/img/raindrop.png') }}" alt="" class="raindrop-icon">指名ランキング</a></li>
