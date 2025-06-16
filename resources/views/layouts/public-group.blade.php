93cb5b54ab97075b52496541954ac1659ec6c620
liu wei <ryu@applink.co.jp>
2025-06-16 13:21:41 +0900

    Typekitのスクリプトをビューに追加し、SCSSファイルに読み込み中と失敗時のスタイルを追加しました。また、不要なスクリプトを削除しました。

--- resources/views/layouts/public-group.blade.php
+++ resources/views/layouts/public-group.blade.php
@@ -9,7 +9,16 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
 
     <title>{{ config('app.name', 'PLOグループ') }}</title>
-
+      <script>
+      (function(d) {
+  var config = {
+    kitId: 'asj2cdd',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);  
+</script>
     <link rel="icon" href="favicon.ico">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Scripts -->
