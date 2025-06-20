cfbe4a3aaa6e303e5620bec35740c4b823b9df32
liu wei <ryu@applink.co.jp>
2025-06-20 18:43:44 +0900

    ニュース詳細ページにヘッダーを追加し、ニュース内容の表示スタイルを更新しました。これにより、ニュースセクションがより視覚的に整ったものになります。

--- resources/views/public/shop/newsdetail.blade.php
+++ resources/views/public/shop/newsdetail.blade.php
@@ -1,6 +1,11 @@
 <x-public-shop-layout :shop="$shop">
 
   <section class="news-detail content-wrapper">
+    <div class="news-detail-header">
+      <div class="news-detail-header-title title-font">
+        NEWS
+      </div>
+    </div>
     <div class="news-detail-published-at">
       <p>{{ $news->published_at ? \Carbon\Carbon::createFromTimeString($news->published_at)->format('y.m.d') : '' }}</p>
     </div>
@@ -10,7 +15,7 @@
     <div class="news-detail-image">
       <img src="{{ asset('storage/' . $news->thumbnail) }}" alt="{{ $news->title }}">
     </div>
-    <div class="news-detail-contents">
+    <div class="news-detail-contents content-font">
       <p>{!! $news->contents !!}</p>
     </div>
   </section>
