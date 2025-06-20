e31418c58f7311220c03fcd29014c59ece98539a
liu wei <ryu@applink.co.jp>
2025-06-20 18:52:20 +0900

    ニュース詳細ページのヘッダーを追加し、タイトルのスタイルを更新しました。また、ニュース内容のフォントサイズとウェイトを修正し、レスポンシブデザインを強化しました。

--- resources/views/public/shop/newsdetail.blade.php
+++ resources/views/public/shop/newsdetail.blade.php
@@ -10,7 +10,7 @@
       <p>{{ $news->published_at ? \Carbon\Carbon::createFromTimeString($news->published_at)->format('y.m.d') : '' }}</p>
     </div>
     <div class="news-detail-title">
-      <h2 class="news-detail-title-ja title-font">{{ $news->title }}</h2>
+      <h2 class="news-detail-title-ja">{{ $news->title }}</h2>
     </div>
     <div class="news-detail-image">
       <img src="{{ asset('storage/' . $news->thumbnail) }}" alt="{{ $news->title }}">
