6470116682d82bb53158bc8f154ab7771c096698
liuwei9208 <ryu@applink.co.jp>
2025-07-07 18:05:05 +0900

    Add logging for cast and messages in DiaryService fetch method
    - Introduced logging for the fetched cast and messages to aid in debugging and monitoring.
    - Ensured that the logging captures relevant information for better traceability.

--- app/Services/DiaryService.php
+++ app/Services/DiaryService.php
@@ -16,13 +16,14 @@ class DiaryService
     public static function fetch(string $cast_id)
     {
         $cast = Cast::find($cast_id);
+        Log::info('cast', [$cast]);
 
         if (!$cast || !$cast->diary_email_to || !$cast->diary_email_password) {
             return self::FETCH_RESULT_FAILED;
         }
 
         $messages = self::getMessages($cast);
-
+        Log::info('messages', [$messages]);
         foreach ($messages as $message) {
             if (self::isStored($cast_id, $message)) {
                 continue;
