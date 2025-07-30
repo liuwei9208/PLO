9fb1a01ab1be7ef08a211465dcffc0c4c499c513
liuwei9208 <ryu@applink.co.jp>
2025-07-30 12:24:26 +0900

    Update ReviewController to utilize History model for review creation and clean up ReviewController index method
    - Integrated History model to fetch cast_id during review creation.
    - Removed commented-out SQL query in the index method for clarity.
    - Minor formatting adjustments for improved readability.

--- app/Http/Controllers/Admin/ReviewController.php
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -30,7 +30,7 @@ class ReviewController extends Controller
         } else {
             $query = Review::query();
         }
-        
+
         if ($request->has('search') && $request->query('search')) {
             $search = '%' . $request->query('search') . '%';
             $query->whereHas('cast', function ($query) use ($search) {
@@ -59,7 +59,38 @@ class ReviewController extends Controller
         $reviews = $query->skip($skip)
             ->take($limit)
             ->orderBy('created_at', 'desc')->get();
-
+        // $sql = 'SELECT `'.env("DB_DATABASE").'`.reviews.id as review_id,
+        //     `'.env("DB_DATABASE").'`.reviews.title as review_title,
+        //     `'.env("DB_DATABASE").'`.reviews.content as review_content,
+        //     `'.env("DB_DATABASE").'`.reviews.created_at as review_created_at,
+        //     `'.env("DB_DATABASE").'`.reviews.is_public as review_is_public,
+        //     `'.env("DB_DATABASE").'`.reviews.member_id as review_member_id,
+        //     `'.env("DB_DATABASE").'`.reviews.history_id as review_history_id,
+        //     `'.env("DB_DATABASE").'`.reviews.average_point as review_average_point,
+        //     `'.env("DB_DATABASE").'`.reviews.cast_point as review_cast_point,
+        //     `'.env("DB_DATABASE").'`.reviews.play_point as review_play_point,
+        //     `'.env("DB_DATABASE").'`.reviews.price_point as review_price_point,
+        //     `'.env("DB_DATABASE").'`.reviews.stuff_point as review_stuff_point,
+        //     `'.env("DB_DATABASE").'`.reviews.photo_point as review_photo_point,
+        //     `'.env("DB_DATABASE").'`.reviews.manager_comment as review_manager_comment,
+        //     `'.env("DB_DATABASE").'`.members.name as member_name,
+        //     `'.env("DB_DATABASE").'`.casts.id as cast_id,
+        //     `'.env("DB_DATABASE").'`.casts.name as cast_name,
+        //     `'.env("DB_DATABASE").'`.casts.age as cast_age,
+        //     `'.env("DB_DATABASE").'`.casts.height as cast_height,
+        //     `'.env("DB_DATABASE").'`.casts.bra_size as cast_cup,
+        //     `'.env("DB_DATABASE").'`.casts.bust as cast_bust,
+        //     `'.env("DB_DATABASE").'`.casts.waist as cast_waist,
+        //     `'.env("DB_DATABASE").'`.casts.hip as cast_hip,
+        //     `'.env("DB_DATABASE").'`.casts.gallery_1 as cast_gallery
+        //     FROM `'.env("DB_DATABASE").'`.reviews
+        //     LEFT JOIN `'.env("MEMBER_DB_DATABASE").'`.histories ON `'.env("DB_DATABASE").'`.reviews.history_id = `'.env("MEMBER_DB_DATABASE").'`.histories.id
+        //     LEFT JOIN `'.env("DB_DATABASE").'`.members ON `'.env("DB_DATABASE").'`.reviews.member_id = `'.env("DB_DATABASE").'`.members.id
+        //     LEFT JOIN `'.env("DB_DATABASE").'`.casts ON `'.env("MEMBER_DB_DATABASE").'`.histories.cast_id = `'.env("DB_DATABASE").'`.casts.id
+        //     WHERE `'.env("MEMBER_DB_DATABASE").'`.histories.shop_id = '.Shop::where('slug', $shop)->first()->id.'
+        //     AND `'.env("DB_DATABASE").'`.casts.is_public = 1
+        //     AND `'.env("DB_DATABASE").'`.reviews.is_public = 1 ORDER BY `'.env("DB_DATABASE").'`.reviews.created_at DESC';
+        // dd($reviews[0]->cast);
         return view('admin.review.index', [
             'reviews' => $reviews,
             'page' => $page,
