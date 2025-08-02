b348f411a9f8482debf693eda556b3a1bac878bd
kimdaeseung208 <kimdaeseung208@gmail.com>
2025-08-03 04:01:09 +0900

    Enhance header functionality and styling
    - Added conditional rendering for header based on route.
    - Introduced new header-child structure for improved user navigation.
    - Updated JavaScript to dynamically adjust logo positioning and drawer toggle based on logo height.
    - Modified SCSS to define CSS variables for logo positioning and adjusted z-index for header elements.

--- resources/views/components/public/group/header-front.blade.php
+++ resources/views/components/public/group/header-front.blade.php
@@ -1,3 +1,4 @@
+@if (request()->routeIs('public.group.home'))
 <header class="header" id="header">
   <a href="{{ route('public.group.home') }}" class="header-logo sm">
     <img src="{{ asset('assets/img/group/header/plo-logo.png') }}" alt="">
@@ -61,6 +62,84 @@
   </div>
 
 </header>
+@else
+<header class="header-child" id="header">
+  <a href="{{ route('public.group.home') }}" class="header-child-logo sm">
+    <img src="{{ asset('assets/img/group/header/plo-logo.png') }}" alt="">
+  </a>
+  <div class="header-child-user md lg">
+    <div class="header-child-user-logo">
+      <img src="{{ asset('assets/img/group/header/plo-logo.png') }}" alt="">
+    </div>
+    <div class="header-child-user-menu">
+      <a href="{{ route('public.group.newcomer') }}">
+        <small>TOP</small>
+        <span class="menu_top">トップページ</span>
+      </a>
+      <a href="{{ route('public.group.newcomer') }}">
+        <small>NEWFACET</small>
+        <span class="menu_newface">新人情報</span>
+      </a>
+      <a href="{{ route('public.group.schedule') }}">
+        <small>SCHEDULE</small>
+        <span class="menu_schedule">出勤情報</span>
+      </a>
+      <a href="{{ route('public.group.home') }}" class="header-child-user-menu-logo">
+        <small>plo</small>
+        <span class="menu_schedule">plo</span>
+        {{-- <div class="header-child-user-menu-logo-img">
+          <img src="{{ asset('assets/img/group/header/plo-logo.png') }}" alt="">
+        </div> --}}
+      </a>
+      <a href="{{ route('public.group.event') }}">
+        <small>EVENT</small>
+        <span class="menu_event">イベント情報</span>
+      </a>
+      <a href="{{ route('public.group.shop') }}">
+        <small>SHOP</small>
+        <span class="menu_shop">店舗一覧</span>
+      </a>
+    </div>
+    <div class="header-child-user-logbox">
+    @if (Auth::guard('member')->check() || Auth::guard('web')->check())
+      <a href="{{ route('logoutAll') }}">
+        <small>LOGOUT</small>
+        <span>ログアウト</span>
+      </a>
+      @if (Auth::guard('member')->check())
+      {{-- <div class="header-child-user-logbox-last">
+        <span></span> --}}
+        <a href="{{ route('public.group.mypage') }}">
+          <small>MYPAGE</small>
+          <span>マイページ</span>
+        </a>
+      {{-- </div> --}}
+      @elseif (Auth::guard('web')->check())
+      {{-- <div class="header-child-user-logbox-last"> --}}
+        <span></span>
+        <a href="{{ route('admin.home') }}">
+          <small>MANAGER</small>
+          <span>管理者</span>
+        </a>
+      {{-- </div> --}}
+      @endif
+    @else
+      <a href="{{ route('login') }}">
+        <small>LOGIN</small>
+        <span>ログイン</span>
+      </a>
+      {{-- <div class="header-child-user-logbox-last">
+        <span></span> --}}
+        <a href="{{ route('terms.show') }}">
+          <small>SIGN UP</small>
+          <span>新規登録</span>
+        </a>
+      {{-- </div> --}}
+    @endif
+    </div>
+  </div>  
+</header>
+@endif
 <button class="drawer-toggle" id="drawer-toggle" data-pushbar-target="right">
   <div class="drawer-toggle-bars">
     <span class="drawer-toggle-bar"></span>
