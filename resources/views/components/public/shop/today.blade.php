b9c59399b8dbb5304184a63a0b7d4ce1792548d6
kimdaeseung208 <kimdaeseung208@gmail.com>
2025-08-03 18:41:43 +0900

    🔒️ スマホ版の店舗TOP、出勤は1列ではなく、2列に 店舗版の出勤・新人キャストにカーソル合わせた時のコメント吹き出し削除し、待機情報みたいに少し白くなる演出に変更

--- resources/views/components/public/shop/today.blade.php
+++ resources/views/components/public/shop/today.blade.php
@@ -5,11 +5,12 @@
   <div class="today-photo">
     <img src="{{ asset('storage/' . $cast->gallery_1) }}" alt="{{ $cast->name }}">
     <div class="today-status --{{ $cast->shop_slug }}">
-      <img src="{{ asset('assets/img/shop/clock-icon-y.png') }}" alt="本日出勤">
-      <span class="today-status-label">本日出勤</span>
+      <img src="{{ asset('assets/img/shop/clock-icon-y.png') }} " class="pc-only" alt="本日出勤">
+      <span class="today-status-label pc-only">本日出勤</span>
+      <span class="today-status-label sp-only">出勤</span>
       <span class="today-status-time --{{ $cast->shop_slug }}">{{ date('H:i', strtotime($cast->start_datetime)) }} ～ {{ date('H:i', strtotime($cast->end_datetime)) }}</span>
     </div>
-    <div class="today-profile">
+    <div class="today-profile pc-only">
       <span class="today-profile-schedule">
         <img src="{{ asset('assets/img/shop/clock-icon-y.png') }}" alt="移動時間のみ">
         待機中
@@ -23,7 +24,17 @@
         T{{ $cast->height }} B{{ $cast->bust }} W{{ $cast->waist }} H{{ $cast->hip }}
       </span>
     </div>
-    <div class="today-hover">
+    <div class="today-profile-sp sp-only">
+      <span class="today-profile-sp-name --{{ $cast->shop_slug }}">
+        {{-- <img src="{{ asset('assets/img/shop/star-w.png') }}" alt="移動時間のみ"> --}}
+        {{ $cast->name }}({{ $cast->age }})
+      </span>
+      <span class="today-profile-sp-size ">
+        {{-- <img src="{{ asset('assets/img/shop/heart-w.png') }}" alt="移動時間のみ"> --}}
+        T{{ $cast->height }} B{{ $cast->bust }} W{{ $cast->waist }} H{{ $cast->hip }}
+      </span>
+    </div>
+    {{-- <div class="today-hover">
       <div class="today-hover-name">
           {{ $cast->name }}
       </div>
@@ -32,7 +43,7 @@
           {{ $cast->appeal_point }}
         </div>
       </div>
-    </div>
+    </div> --}}
   </div>
   {{-- <div class="today-status --{{ $cast->shop_slug }}">
     <img src="{{ asset('assets/img/shop/clock-icon-y.png') }}" alt="本日出勤">
