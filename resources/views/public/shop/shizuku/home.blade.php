1454034925406d27c85516a7382d5f829ffd89ca
JinHwangHae <jin.dev970921@outlook.com>
2025-11-24 07:21:50 +0800

    add top-page button

--- resources/views/public/shop/shizuku/home.blade.php
+++ resources/views/public/shop/shizuku/home.blade.php
@@ -97,6 +97,16 @@
                         </svg>                    
                     <p>menu</p>
                 </div>
+                <!-- Page Top Button -->
+                <div class="page-top-section">
+                    <div class="page-top-line"></div>
+                    <a href="#" class="page-top-text" onclick="window.scrollTo({top: 0, behavior: 'smooth'}); return false;">page top</a>
+                    <a href="#" class="page-top-text" onclick="window.scrollTo({top: 0, behavior: 'smooth'}); return false;" class="page-top-social">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                            <path d="M5.71429 0C2.5625 0 0 2.5625 0 5.71429V34.2857C0 37.4375 2.5625 40 5.71429 40H34.2857C37.4375 40 40 37.4375 40 34.2857V5.71429C40 2.5625 37.4375 0 34.2857 0H5.71429ZM32.2411 7.5L22.9732 18.0893L33.875 32.5H25.3393L18.6607 23.7589L11.0089 32.5H6.76786L16.6786 21.1696L6.22321 7.5H14.9732L21.0179 15.4911L28 7.5H32.2411ZM28.8661 29.9643L13.6964 9.90179H11.1696L26.5089 29.9643H28.8571H28.8661Z" fill="#B19D67"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
             <div class="home-schedule">
                 <div class="home-schedule-title">
