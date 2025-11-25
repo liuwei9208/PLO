3f78567f3c1f51454d897b19c6d2f55d6176b001
liu wei <liu-wei@attracttojapan.jp>
2025-11-25 12:51:30 +0900

    merge

--- resources/views/layouts/shizuku.blade.php
+++ resources/views/layouts/shizuku.blade.php
@@ -8,12 +8,11 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<<<<<<< HEAD
-=======
     <link href="https://fonts.googleapis.com/css2?family=Lustria&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
->>>>>>> 27eb87e50876f9f55243489ba9fa7de65433068d
+    <link
+        href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
     {{-- <link
         href="https://fonts.googleapis.com/css2?family=Bentham&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet"> --}}
