f339d9419160189d5bf8ad401a5faea577a8fc78
liu wei <liu-wei@attracttojapan.jp>
2025-12-02 16:48:53 +0900

    modify bottom menu, banner

--- resources/views/public/shop/shizuku/home.blade.php
+++ resources/views/public/shop/shizuku/home.blade.php
@@ -84,7 +84,7 @@
                     @for ($i = 0; $i < 12; $i++)
                         <x-public.shops.schedule-card background-image="assets/img/shops/shizuku/coming-soon-card.png"
                             frame-image="assets/img/shops/shizuku/card-frame.png" badge-shift="本日出勤"
-                            badge-time="12:00〜24:00"
+                            badge-time="12:00~24:00"
                             status-icon='<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.5 0C5.6075 0 0 5.6075 0 12.5C0 19.3925 5.6075 25 12.5 25C19.3925 25 25 19.3925 25 12.5C25 5.6075 19.3925 0 12.5 0ZM19.6875 13.75H11.25V5H13.75V11.25H19.6875V13.75Z" fill="#FFE600"/></svg>'
                             status-text="待機中" name="のんたん（20）" measurements="T.160 B.85(C) W.60 H.83"
                             message="キャストメッセージが出ます" />
