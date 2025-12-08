16fa5e0763a62a2c4d3d86d25b6aaf9e869be1be
liu wei <liu-wei@attracttojapan.jp>
2025-12-08 20:32:54 +0900

    Enhance ShizukuController to include shop-specific data in the access view, improving content presentation. Update access Blade template to utilize dynamic data for address, phone number, hours, and additional information. Modify access styles for better layout and responsiveness, including a Google Maps iframe integration for location display.

--- resources/views/public/shop/shizuku/access.blade.php
+++ resources/views/public/shop/shizuku/access.blade.php
@@ -1,35 +1,33 @@
-<x-shizuku-page-layout
-    page-title="ACCESS"
-    page-subtitle="アクセス情報"
-    breadcrumb="すすきのhigh grade health 雫 ＞ トップページ ＞ アクセス情報"
-    :assets="[
-        'resources/scss/shops/shizuku/access.scss',
-    ]"
->
+<x-shizuku-page-layout page-title="ACCESS" page-subtitle="アクセス情報" breadcrumb="すすきのhigh grade health 雫 ＞ トップページ ＞ アクセス情報"
+    :assets="['resources/scss/shops/shizuku/access.scss']" :banners="$banners">
     <section class="access-section">
         <h1 class="access-title">ABOUT</h1>
         <div class="access-content">
             <div class="access-content-item">
                 <h2> 住所 </h2>
-                <p> 北海道札幌市中央区南5条西5丁目 第8旭観光ビル </p>
+                <p> {{ $shop_item->address1 . '　　' . $shop_item->address2 }} </p>
             </div>
             <div class="access-content-item">
                 <h2> 電話番号 </h2>
-                <p> 011-521-3593</p>
+                <p> {{ $shop_item->tel }} </p>
             </div>
             <div class="access-content-item">
                 <h2> 営業時間 </h2>
-                <p> 09:00〜00:00 </p>
+                <p> {{ $shop_item->open_start->format('H:i') . '〜' . $shop_item->open_end->format('H:i') }} </p>
             </div>
             <div class="access-content-item">
                 <h2> 店舗情報 </h2>
-                <p> テスト１ </p>
+                <p> {{ $shop_item->memo }} </p>
             </div>
         </div>
         <h1 class="map-title">MAP</h1>
         <div class="map-content">
-            <img src="{{ asset('assets/img/shops/shizuku/map.png') }}" alt="Map" class="pc-only">
-            <img src="{{ asset('assets/img/shops/shizuku/map-sp.png') }}" alt="Map" class="sp-only">
+            <iframe style="border:0" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps?q={{ $shop_item->address1 . ' ' . $shop_item->address2 }}&output=embed">
+            </iframe>
+
+            {{-- <img src="{{ asset('assets/img/shops/shizuku/map.png') }}" alt="Map" class="pc-only">
+            <img src="{{ asset('assets/img/shops/shizuku/map-sp.png') }}" alt="Map" class="sp-only"> --}}
         </div>
     </section>
 </x-shizuku-page-layout>
--- resources/views/public/shop/shizuku/photo-diary.blade.php
+++ resources/views/public/shop/shizuku/photo-diary.blade.php
@@ -1,15 +1,9 @@
 @push('styles')
-<link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.css" rel="stylesheet">
 @endpush
 
-<x-shizuku-page-layout
-    page-title="PHOTO DIARY"
-    page-subtitle="写メ日記"
-    breadcrumb="すすきのhigh grade health 雫 ＞ トップページ ＞ 写メ日記"
-    :assets="[
-        'resources/scss/shops/shizuku/photo-diary.scss',
-    ]"
->
+<x-shizuku-page-layout page-title="PHOTO DIARY" page-subtitle="写メ日記" breadcrumb="すすきのhigh grade health 雫 ＞ トップページ ＞ 写メ日記"
+    :assets="['resources/scss/shops/shizuku/photo-diary.scss']">
     <section class="photo-diary-section">
         <div class="diary-body-left-calendar pc-only">
             <div class="diary-body-left-calendar-content" id="diary-calendar">
@@ -17,54 +11,58 @@
         </div>
         <div class="diary-body-right-content">
             <div class="diary-post-cards">
-                @foreach(range(1, 4) as $index)
-                <div class="diary-post-card pc-only">
-                    <div class="diary-post-title">
-                        <h1>投稿者名</h1>
-                        <span>ブログタイトル</span>
-                        <hr></hr>
-                        <p>00.00.00.</p>
-                    </div>
-                    <div class="diary-post-image">
-                        <img src="{{ asset('assets/img/shops/shizuku/diary-image2.png') }}">
-                    </div>
-                    <div class="diary-post-content">
-                        <p>ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります<br><br>
-                            ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本
-                        </p>
-                    </div>
-                </div>
-                <div class="sp-only">
-                    <div class="diary-post-sp-card">
-                        <div class="diary-post-image">
-                            <img src="{{ asset('assets/img/shops/shizuku/diary-image2.png') }}">
-                        </div>
+                @foreach (range(1, 4) as $index)
+                    <div class="diary-post-card pc-only">
                         <div class="diary-post-title">
                             <h1>投稿者名</h1>
                             <span>ブログタイトル</span>
-                            <hr></hr>
+                            <hr>
+                            </hr>
                             <p>00.00.00.</p>
-                            <div class="diary-post-content">
-                                <p>ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります<br><br>
-                                    ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本
-                                </p>
+                        </div>
+                        <div class="diary-post-image">
+                            <img src="{{ asset('assets/img/shops/shizuku/diary-image.png') }}">
+                        </div>
+                        <div class="diary-post-content">
+                            <p>ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります<br><br>
+                                ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sp-only">
+                        <div class="diary-post-sp-card">
+                            <div class="diary-post-image">
+                                <img src="{{ asset('assets/img/shops/shizuku/diary-image2.png') }}">
+                            </div>
+                            <div class="diary-post-title">
+                                <h1>投稿者名</h1>
+                                <span>ブログタイトル</span>
+                                <hr>
+                                </hr>
+                                <p>00.00.00.</p>
+                                <div class="diary-post-content">
+                                    <p>ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります<br><br>
+                                        ブログ本文が入りますブログ本文が入りますブログ本文が入りますブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本文が入ります ブログ本
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
             <div class="diary-pagination pc-only">
                 <nav class="page-navigation">
                     <a href="#" class="page-nav-btn prev" aria-label="Previous">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none">
                             <g clip-path="url(#clip0_559_4)">
-                              <path d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z" fill="#2A1A08"/>
+                                <path d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z"
+                                    fill="#2A1A08" />
                             </g>
                             <defs>
-                              <clipPath id="clip0_559_4">
-                                <rect width="24" height="24" fill="white"/>
-                              </clipPath>
+                                <clipPath id="clip0_559_4">
+                                    <rect width="24" height="24" fill="white" />
+                                </clipPath>
                             </defs>
                         </svg>
                     </a>
@@ -72,14 +70,17 @@
                     <a href="#" class="page-number">2</a>
                     <a href="#" class="page-number">3</a>
                     <a href="#" class="page-nav-btn next" aria-label="Next">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none">
                             <g clip-path="url(#clip0_559_17)">
-                              <path d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z" fill="#2A1A08"/>
+                                <path
+                                    d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z"
+                                    fill="#2A1A08" />
                             </g>
                             <defs>
-                              <clipPath id="clip0_559_17">
-                                <rect width="24" height="24" fill="white"/>
-                              </clipPath>
+                                <clipPath id="clip0_559_17">
+                                    <rect width="24" height="24" fill="white" />
+                                </clipPath>
                             </defs>
                         </svg>
                     </a>
@@ -90,14 +91,15 @@
     <div class="diary-pagination sp-only">
         <nav class="page-navigation">
             <a href="#" class="page-nav-btn prev" aria-label="Previous">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none">
                     <g clip-path="url(#clip0_559_4)">
-                      <path d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z" fill="#2A1A08"/>
+                        <path d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z" fill="#2A1A08" />
                     </g>
                     <defs>
-                      <clipPath id="clip0_559_4">
-                        <rect width="24" height="24" fill="white"/>
-                      </clipPath>
+                        <clipPath id="clip0_559_4">
+                            <rect width="24" height="24" fill="white" />
+                        </clipPath>
                     </defs>
                 </svg>
             </a>
@@ -105,14 +107,16 @@
             <a href="#" class="page-number">2</a>
             <a href="#" class="page-number">3</a>
             <a href="#" class="page-nav-btn next" aria-label="Next">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none">
                     <g clip-path="url(#clip0_559_17)">
-                      <path d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z" fill="#2A1A08"/>
+                        <path d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z"
+                            fill="#2A1A08" />
                     </g>
                     <defs>
-                      <clipPath id="clip0_559_17">
-                        <rect width="24" height="24" fill="white"/>
-                      </clipPath>
+                        <clipPath id="clip0_559_17">
+                            <rect width="24" height="24" fill="white" />
+                        </clipPath>
                     </defs>
                 </svg>
             </a>
@@ -124,12 +128,12 @@
     </div>
 </x-shizuku-page-layout>
 <script>
-  let cast_id = "";
-  let shop_id = "";
-  let shop_slug = "shizuku";
-  let date = "";
-  let diarys_date = [];
+    let cast_id = "";
+    let shop_id = "";
+    let shop_slug = "shizuku";
+    let date = "";
+    let diarys_date = [];
 </script>
 @once
-  @vite(['resources/scss/shops/shizuku/photo-diary.scss','resources/js/shops/shizuku/photo-diary.js'])
+    @vite(['resources/scss/shops/shizuku/photo-diary.scss', 'resources/js/shops/shizuku/photo-diary.js'])
 @endonce
