cf5ff1e4d8c885fded83f96b8c492e0df4f86671
JinHwangHae <jin.dev970921@outlook.com>
2025-12-09 09:40:42 +0800

    Add event section slider functionality and styles
    Implement a new JavaScript file for the event section that initializes a main banner slider and a sub-banners slider using Swiper. Update the Blade template to conditionally render the main and sub-banners based on the presence of multiple images. Enhance SCSS styles for the event section, including responsive adjustments and navigation button styles for both sliders.

--- resources/views/components/public/shops/event-section.blade.php
+++ resources/views/components/public/shops/event-section.blade.php
@@ -11,19 +11,67 @@
         <img src="{{ asset($backgroundImage) }}" alt="{{ $backgroundAlt }}" class="event-bg-image">
         <div class="event-bg-overlay"></div>
     </div>
+    @php
+        // Use subBannerImages for main banner if provided, otherwise use mainBannerImage
+        $mainBannerImages = count($subBannerImages) > 0 ? $subBannerImages : [['image' => $mainBannerImage, 'alt' => $mainBannerAlt]];
+        $hasMultipleImages = count($mainBannerImages) > 1;
+    @endphp
     <div class="event-main-banner">
         <div class="event-main-bg">
-            <img src="{{ asset($mainBannerImage) }}" alt="{{ $mainBannerAlt }}" class="event-main-image">
+            @if($hasMultipleImages)
+                <div class="event-main-banner-slider swiper">
+                    <div class="swiper-wrapper">
+                        @foreach($mainBannerImages as $banner)
+                            <div class="swiper-slide">
+                                <img src="{{ asset($banner['image'] ?? $banner) }}" alt="{{ $banner['alt'] ?? $mainBannerAlt }}" class="event-main-image">
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <button class="event-main-banner-prev">
+                    <img src="{{ asset('assets/img/group/newface/prev.svg') }}" alt="Previous">
+                </button>
+                <button class="event-main-banner-next">
+                    <img src="{{ asset('assets/img/group/newface/next.svg') }}" alt="Next">
+                </button>
+            @else
+                <img src="{{ asset($mainBannerImage) }}" alt="{{ $mainBannerAlt }}" class="event-main-image">
+            @endif
         </div>
     </div>
     @if(count($subBannerImages) > 0)
         <div class="event-sub-banners">
-            @foreach($subBannerImages as $subBanner)
-                <div class="event-sub-banner">
-                    <img src="{{ asset($subBanner['image'] ?? $subBanner) }}" alt="{{ $subBanner['alt'] ?? 'Event Sub Banner' }}">
+            @if($hasMultipleImages)
+                <div class="event-sub-banners-slider swiper">
+                    <div class="swiper-wrapper">
+                        @foreach($subBannerImages as $index => $subBanner)
+                            <div class="swiper-slide" data-swiper-slide-index="{{ $index }}">
+                                <div class="event-sub-banner">
+                                    <img src="{{ asset($subBanner['image'] ?? $subBanner) }}" alt="{{ $subBanner['alt'] ?? 'Event Sub Banner' }}">
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
-            @endforeach
+                <button class="event-sub-banners-prev">
+                    <img src="{{ asset('assets/img/group/newface/prev.svg') }}" alt="Previous">
+                </button>
+                <button class="event-sub-banners-next">
+                    <img src="{{ asset('assets/img/group/newface/next.svg') }}" alt="Next">
+                </button>
+            @else
+                <div class="event-sub-banners-static">
+                    @foreach($subBannerImages as $subBanner)
+                        <div class="event-sub-banner">
+                            <img src="{{ asset($subBanner['image'] ?? $subBanner) }}" alt="{{ $subBanner['alt'] ?? 'Event Sub Banner' }}">
+                        </div>
+                    @endforeach
+                </div>
+            @endif
         </div>
     @endif
 </div>
 
+@once
+    @vite(['resources/js/shops/event-section.js'])
+@endonce
