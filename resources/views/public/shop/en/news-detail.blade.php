fe2f31a3272e4a893bb47976eeddb87c6bd53242
liuwei9208 <ryu@applink.co.jp>
2026-01-09 14:45:16 +0900

    ✨ Update news detail styles and Blade templates for the English shop, enhancing visual consistency by adjusting colors, borders, and SVG icon fills across various screen sizes.

--- resources/views/public/shop/en/news-detail.blade.php
+++ resources/views/public/shop/en/news-detail.blade.php
@@ -26,12 +26,22 @@
         <div class="news-detail-pagination-left">
             @if ($prevNews)
                 <a href="{{ route('public.shops.shop.news.detail', ['shop' => $shop->slug, 'id' => $prevNews->id]) }}">
+                  <div class="pc-only">
                     <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
                         fill="none">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M0.292282 7.071L5.94928 12.728L7.36328 11.314L2.41328 6.364L7.36328 1.414L5.94928 0L0.292282 5.657C0.104811 5.84453 -0.000504971 6.09884 -0.000504971 6.364C-0.000504971 6.62916 0.104811 6.88347 0.292282 7.071Z"
-                            fill="#010307" />
+                            fill="#FFF" />
                     </svg>
+                  </div>
+                  <div class="sp-only">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                        fill="none">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M0.292282 7.071L5.94928 12.728L7.36328 11.314L2.41328 6.364L7.36328 1.414L5.94928 0L0.292282 5.657C0.104811 5.84453 -0.000504971 6.09884 -0.000504971 6.364C-0.000504971 6.62916 0.104811 6.88347 0.292282 7.071Z"
+                            fill="#0D0709" />
+                    </svg>
+                  </div>
                     &nbsp;{{ $prevNews->title }}
                 </a>
             @else
@@ -51,12 +61,22 @@
             @if ($nextNews)
                 <a href="{{ route('public.shops.shop.news.detail', ['shop' => $shop->slug, 'id' => $nextNews->id]) }}">
                     {{ $nextNews->title }}&nbsp;
+                  <div class="pc-only">
                     <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
                         fill="none">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M7.071 7.071L1.414 12.728L0 11.314L4.95 6.364L0 1.414L1.414 0L7.071 5.657C7.25847 5.84453 7.36379 6.09884 7.36379 6.364C7.36379 6.62916 7.25847 6.88347 7.071 7.071Z"
-                            fill="#010307" />
+                            fill="#FFF" />
                     </svg>
+                  </div>
+                  <div class="sp-only">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                        fill="none">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M7.071 7.071L1.414 12.728L0 11.314L4.95 6.364L0 1.414L1.414 0L7.071 5.657C7.25847 5.84453 7.36379 6.09884 7.36379 6.364C7.36379 6.62916 7.25847 6.88347 7.071 7.071Z"
+                            fill="#0D0709" />
+                    </svg>
+                  </div>
                 </a>
             @else
                 {{-- <span class="news-detail-pagination-disabled">
