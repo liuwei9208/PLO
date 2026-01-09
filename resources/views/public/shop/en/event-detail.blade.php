53faccfc3188015d1eb9066387dae76665139701
liuwei9208 <ryu@applink.co.jp>
2026-01-09 15:01:08 +0900

    ✨ Update styles and Blade templates for the English shop's event pages, enhancing visual consistency by adjusting background colors, borders, and SVG icon fills, while correcting asset paths for improved layout coherence.

--- resources/views/public/shop/en/event-detail.blade.php
+++ resources/views/public/shop/en/event-detail.blade.php
@@ -1,5 +1,5 @@
-<x-shiroganeze-page-layout -page-layout page-title="EVENT" page-subtitle="イベント情報"
-    breadcrumb="すすきの Premium Men’s Esthe シロガネーゼ ＞ トップページ ＞ イベント情報 ＞ {{ $event->title }}" :assets="['resources/scss/shops/shiroganeze/event-detail.scss']"
+<x-en-page-layout -page-layout page-title="EVENT" page-subtitle="イベント情報"
+    breadcrumb="fashion health 艶 ＞ トップページ ＞ イベント情報 ＞ {{ $event->title }}" :assets="['resources/scss/shops/en/event-detail.scss']"
     :banners="$banners">
     <section class="event-detail-section">
         <div class="event-detail-card">
@@ -20,12 +20,22 @@
             @if ($prevEvent)
                 <a
                     href="{{ route('public.shops.shop.event.detail', ['shop' => $shop->slug, 'id' => $prevEvent->id]) }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
-                        fill="none">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M0.292282 7.071L5.94928 12.728L7.36328 11.314L2.41328 6.364L7.36328 1.414L5.94928 0L0.292282 5.657C0.104811 5.84453 -0.000504971 6.09884 -0.000504971 6.364C-0.000504971 6.62916 0.104811 6.88347 0.292282 7.071Z"
-                            fill="#010307" />
-                    </svg>
+                    <div class="pc-only">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                          fill="none">
+                          <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M0.292282 7.071L5.94928 12.728L7.36328 11.314L2.41328 6.364L7.36328 1.414L5.94928 0L0.292282 5.657C0.104811 5.84453 -0.000504971 6.09884 -0.000504971 6.364C-0.000504971 6.62916 0.104811 6.88347 0.292282 7.071Z"
+                              fill="#EBEBEB" />
+                      </svg>
+                    </div>
+                    <div class="sp-only">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                          fill="none">
+                          <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M0.292282 7.071L5.94928 12.728L7.36328 11.314L2.41328 6.364L7.36328 1.414L5.94928 0L0.292282 5.657C0.104811 5.84453 -0.000504971 6.09884 -0.000504971 6.364C-0.000504971 6.62916 0.104811 6.88347 0.292282 7.071Z"
+                              fill="#0D0709" />
+                      </svg>
+                    </div>
                     &nbsp;{{ $prevEvent->title }}
                 </a>
             @else
@@ -46,12 +56,22 @@
                 <a
                     href="{{ route('public.shops.shop.event.detail', ['shop' => $shop->slug, 'id' => $nextEvent->id]) }}">
                     {{ $nextEvent->title }}&nbsp;
-                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
-                        fill="none">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M7.071 7.071L1.414 12.728L0 11.314L4.95 6.364L0 1.414L1.414 0L7.071 5.657C7.25847 5.84453 7.36379 6.09884 7.36379 6.364C7.36379 6.62916 7.25847 6.88347 7.071 7.071Z"
-                            fill="#010307" />
-                    </svg>
+                    <div class="pc-only">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                          fill="none">
+                          <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M7.071 7.071L1.414 12.728L0 11.314L4.95 6.364L0 1.414L1.414 0L7.071 5.657C7.25847 5.84453 7.36379 6.09884 7.36379 6.364C7.36379 6.62916 7.25847 6.88347 7.071 7.071Z"
+                              fill="#EBEBEB" />
+                      </svg>
+                    </div>
+                    <div class="sp-only">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"
+                          fill="none">
+                          <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M7.071 7.071L1.414 12.728L0 11.314L4.95 6.364L0 1.414L1.414 0L7.071 5.657C7.25847 5.84453 7.36379 6.09884 7.36379 6.364C7.36379 6.62916 7.25847 6.88347 7.071 7.071Z"
+                              fill="#0D0709" />
+                      </svg>
+                    </div>
                 </a>
             @else
                 {{-- <span class="event-detail-pagination-disabled">
@@ -66,4 +86,4 @@
             @endif
         </div>
     </div>
-</x-shiroganeze-page-layout>
+</x-en-page-layout>
--- resources/views/public/shop/en/event.blade.php
+++ resources/views/public/shop/en/event.blade.php
@@ -1,10 +1,10 @@
-<x-shiroganeze-page-layout page-title="EVENT" page-subtitle="イベント情報"
-    breadcrumb="すすきの Premium Men’s Esthe シロガネーゼ ＞ トップページ ＞ イベント情報" :assets="['resources/scss/shops/shiroganeze/event.scss']" :banners="$banners">
+<x-en-page-layout page-title="EVENT" page-subtitle="イベント情報"
+    breadcrumb="fashion health 艶 ＞ トップページ ＞ イベント情報" :assets="['resources/scss/shops/en/event.scss']" :banners="$banners">
     <section class="event-section">
         @foreach ($events as $event)
             <x-public.shops.event-card image="{{ asset('storage/' . $event->thumbnail) }}" image-alt="event-image"
-                title="{{ $event->title }}" :url="route('public.shops.shop.event.detail', ['shop' => 'shiroganeze', 'id' => $event->id])"
-                scss="resources/scss/shops/shiroganeze/component/event-card.scss">
+                title="{{ $event->title }}" :url="route('public.shops.shop.event.detail', ['shop' => 'en', 'id' => $event->id])"
+                scss="resources/scss/shops/en/component/event-card.scss">
                 {!! nl2br($event->contents) !!}
             </x-public.shops.event-card>
         @endforeach
@@ -12,4 +12,4 @@
             {{ $events->links('pagination::shops') }}
         </div>
     </section>
-</x-shiroganeze-page-layout>
+</x-en-page-layout>
